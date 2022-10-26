4e90e8a6144817176ee62142d19190533bf853de
dangvantoan147 <114908210+dangvantoan147@users.noreply.github.com>
2022-10-26 15:58:53 +0700

    Add files via upload

---
+++ bai kiem tra 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	printf("Nhap so nguyen n: ");
+	scanf("%d", &n);
+
+    if(n<=0)
+    printf("So da nhap khong thoa man: ");
+    else {
+    	if(n%2==0){
+    		printf("So da nhap la so chan");
+    		return 1;
+		} 
+    	else 
+    	 printf("So da nhap la so le ");
+         return 0;
+	}
+	getch();
+}
---
+++ bai kiem tra 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int isEvennumber, n, m;
+	printf("Enter a number: ");
+	scanf("%d%d", &n, &m);
+	
+	for(isEvennumber = n ;isEvennumber < m; isEvennumber++){
+		if(isEvennumber%2 == 0){
+			
+			printf("\nThe even number between %d and %d: %d",n , m, isEvennumber );
+		}
+		
+	}
+    return 0;
+}
